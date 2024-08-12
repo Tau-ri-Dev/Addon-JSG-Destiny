c33b14069d611c5ea5cbb4e1c95cb06d6dcc227f
MineDragonCZ_ <minedragoncz@email.cz>
2024-08-12 15:12:18 +0200

    Init

---
+++ src/main/java/dev/tauri/jsgdestinyaddon/JSGDestinyAddon.java
@@ -0,0 +1,8 @@
+package dev.tauri.jsgdestinyaddon;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(JSGDestinyAddon.MOD_ID)
+public class JSGDestinyAddon {
+    public static final String MOD_ID = "jsg_destiny";
+}
