f924282ee82b33b89b4e86e6f1cffb460bf363f6
Fredyman95 <93224497+Fredyman95@users.noreply.github.com>
2025-09-13 20:04:11 +0200

    Added Bulb and Bearing Ball (Crafting elements)

--- src/main/java/dev/tauri/jsgdestiny/common/registry/ItemRegistry.java
+++ src/main/java/dev/tauri/jsgdestiny/common/registry/ItemRegistry.java
@@ -1,15 +1,23 @@
 package dev.tauri.jsgdestiny.common.registry;
 
 import dev.tauri.jsg.helpers.TabHelper;
+import dev.tauri.jsg.item.JSGItem;
+import dev.tauri.jsgdestiny.Constants;
 import dev.tauri.jsgdestiny.JSGDestiny;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class ItemRegistry {
     public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, JSGDestiny.MOD_ID);
 
+    public static final RegistryObject<JSGItem> UNIVERSAL_BULB = Constants.ITEM_HELPER.builder("universal_light_bulb").clearTooltip().setInTabs(List.of(TabRegistry.TAB_DESTINY)).buildGeneric();
+    public static final RegistryObject<JSGItem> BEARING_BALL = Constants.ITEM_HELPER.builder("bearing_ball").clearTooltip().setInTabs(List.of(TabRegistry.TAB_DESTINY)).buildGeneric();
 
     public static void register(IEventBus bus) {
         TabHelper.indexItemRegistry(() -> REGISTER);
